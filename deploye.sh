0f654990723593f6badede28f5e091d29b0f5d72
Suryansh2188 <suryamail4440@gmail.com>
2026-08-24 23:29:38 +0530

    add urls

--- deploye.sh
+++ deploye.sh
@@ -17,7 +17,7 @@ fi
 
 # Check Kubernetes connection
 echo ""
-echo "[1/4] Checking Kubernetes cluster..."
+echo "[1/8] Checking Kubernetes cluster..."
 
 if ! kubectl cluster-info >/dev/null 2>&1; then
     echo "ERROR: Cannot connect to Kubernetes cluster."
@@ -31,7 +31,7 @@ echo "Kubernetes cluster connection successful."
 # Namespace
 # --------------------------------------------------
 echo ""
-echo "[2/4] Applying Namespace..."
+echo "[2/8] Applying Namespace..."
 
 kubectl apply -f "$K8S_DIR/namespace/namespace.yaml"
 
@@ -39,7 +39,7 @@ kubectl apply -f "$K8S_DIR/namespace/namespace.yaml"
 # Secret
 # --------------------------------------------------
 echo ""
-echo "[3/4] Applying Secret..."
+echo "[3/8] Applying Secret..."
 
 kubectl apply -f "$K8S_DIR/secrets/app-secrets.yaml"
 
@@ -47,21 +47,54 @@ kubectl apply -f "$K8S_DIR/secrets/app-secrets.yaml"
 # ConfigMap
 # --------------------------------------------------
 echo ""
-echo "[4/4] Applying ConfigMap..."
+echo "[4/8] Applying ConfigMap..."
 
 kubectl apply -f "$K8S_DIR/configmaps/app-config.yaml"
+
+# --------------------------------------------------
+# Service
+# --------------------------------------------------
+echo ""
+echo "[5/8] Applying Service..."
+
+for file in "$K8S_DIR/services"/*.yaml; do
+    [ -f "$file" ] || continue
+    echo "Applying: $file"
+    kubectl apply -f "$file"
+done
+
 
 # --------------------------------------------------
 # Deployment
 # --------------------------------------------------
 echo ""
-echo "[5/5] Applying Deployment..."
+echo "[6/8] Applying Deployment..."
 
 for file in "$K8S_DIR/deployments"/*.yaml; do
     [ -f "$file" ] || continue
     echo "Applying: $file"
     kubectl apply -f "$file"
 done
+
+# --------------------------------------------------
+# HPA
+# --------------------------------------------------
+echo ""
+echo "[7/8] Applying HPA..."
+
+for file in "$K8S_DIR/hpa"/*.yaml; do
+    [ -f "$file" ] || continue
+    echo "Applying: $file"
+    kubectl apply -f "$file"
+done
+
+# --------------------------------------------------
+# Ingress
+# --------------------------------------------------
+echo ""
+echo "[7/8] Applying ingress..."
+
+kubectl apply -f "$K8S_DIR/ingress/ingress.yaml"
 
 echo ""
 echo "=========================================="
